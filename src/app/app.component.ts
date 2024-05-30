ff32fb9d50f26127bbf6c24a39b021f10a307175
Emi154loya <emi154loya@gmail.com>
2024-05-30 02:49:22 -0600

    feat: first layout design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductService } from './data/services/api/product/product.service';
 import { HttpClientModule } from '@angular/common/http';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule ],
+  imports: [
+    RouterOutlet,
+    HttpClientModule,
+    CardModule,
+    ButtonModule,
+    DialogModule,
+    DropdownModule,
+    InputTextModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'puntodeventa-fe';
+
+  productIncomeModalIsVisible: boolean = false;
+  productOutModalIsVisible: boolean = false;
 
   constructor(private productService: ProductService) {}
 
@@ -22,13 +38,34 @@ export class AppComponent {
     //   console.log(response);
     // });
 
-    this.productService.getProducts().pipe().subscribe(
-      {next:(res) => {
-        console.log(1);
-        console.log(res);
-      }}
-    );
-
+    this.productService
+      .getProducts()
+      .pipe()
+      .subscribe({
+        next: (res) => {
+          console.log(1);
+          console.log(res);
+        },
+      });
   }
 
+  openProductIncomeModal() {
+    this.productIncomeModalIsVisible = true;
+  }
+
+  sendProductIncomeData() {}
+
+  closeProductIncomeModal() {
+    this.productIncomeModalIsVisible = false;
+  }
+
+  sendProductOutData() {}
+
+  openProductOutModal() {
+    this.productOutModalIsVisible = true;
+  }
+
+  closeProductOutModal() {
+    this.productOutModalIsVisible = false;
+  }
 }
